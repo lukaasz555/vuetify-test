7ebc10e4dfb8e3c2f0019bac771f4236df31c669
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-04-01 13:23:51 +0200

    fixed animation bug, added pagination

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,26 +1,33 @@
 <template>
-  <section class="d-flex flex-column flex-sm-row flex-wrap justify-sm-center">
-    <user-card
-      v-for="user in usersStore.users"
-      :key="user.id"
-      :name="user.name"
-      :id="user.id"
-      :email="user.email"
-    ></user-card>
-  </section>
-  <div>
-    <v-select
-      v-model="searchStore.$state.itemsPerPage"
-      :items="pageSizes"
-      @update:model-value="handleItemsPerPageChange"
+  <div class="d-flex flex-column align-center">
+    <section class="d-flex flex-column flex-sm-row flex-wrap justify-sm-center">
+      <user-card
+        v-for="user in usersStore.users"
+        :key="user.id"
+        :name="user.name"
+        :id="user.id"
+        :email="user.email"
+      ></user-card>
+    </section>
+    <div
+      class="d-flex justify-center align-center mt-8"
+      :style="{ maxWidth: '600px' }"
     >
-    </v-select>
-    <v-pagination
-      v-model="searchStore.$state.currentPage"
-      :length="totalPages"
-      color="blue"
-      @update:model-value="handlePageChange"
-    ></v-pagination>
+      <v-select
+        v-model="searchStore.$state.itemsPerPage"
+        :items="pageSizes"
+        @update:model-value="handleItemsPerPageChange"
+        :style="{ maxWidth: '80px' }"
+        :total-visible="5"
+      >
+      </v-select>
+      <v-pagination
+        v-model="searchStore.$state.currentPage"
+        :length="totalPages"
+        color="blue"
+        @update:model-value="handlePageChange"
+      ></v-pagination>
+    </div>
   </div>
 </template>
 
@@ -35,19 +42,15 @@ const searchStore = useSearchStore();
 
 const pageSizes = [4, 8, 12];
 
-onMounted(() => {
-  // console.log(searchStore.$state);
-  usersStore.fetchUsers(searchStore.$state);
-});
+onMounted(() => usersStore.fetchUsers(searchStore.$state));
 
 const totalPages = computed(() => {
-  const res = Math.ceil(searchStore.totalRecords / searchStore.itemsPerPage);
-  console.log(res);
-  return res;
+  return Math.ceil(searchStore.totalRecords / searchStore.itemsPerPage);
 });
 
 function handlePageChange(v: number): void {
   searchStore.$state.currentPage = v;
+  usersStore.fetchUsers(searchStore.$state);
 }
 
 function handleItemsPerPageChange(v: number): void {
